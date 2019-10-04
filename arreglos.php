e27d806166bd2616b7159f304d0a658aa1ac77a9
TheSumsito <mjaral@outlook.com>
2019-10-04 01:43:09 -0300

    Galeria efectos

--- arreglos.php
+++ arreglos.php
@@ -23,7 +23,7 @@
 
 
     <div class="container-gallery">
-        <div class="foto-1">
+        <div class="foto-1" id="foto-1">
             <a href="#img-01"><img src="img/floreros/01.jpg" alt=""></a>
             <div class="descripcion">
                 <h3>hola</h3>
@@ -34,11 +34,11 @@
                     <a href=""><img src="img/floreros/01.jpg" alt=""></a>
                 </div>
                 <div class="cerrar">
-                    <a href="">x</a>
+                    <a href="#">x</a>
                 </div>
             </div>
         </div>
-        <div class="foto-2">
+        <div class="foto-2" id="foto-2">
             <a href="#img-02"><img src="img/floreros/02.jpg" alt=""></a>
             <div class="descripcion">
                 <h3>hola</h3>
@@ -49,11 +49,11 @@
                     <a href=""><img src="img/floreros/02.jpg" alt=""></a>
                 </div>
                 <div class="cerrar">
-                    <a href="">x</a>
+                    <a href="#">x</a>
                 </div>
             </div>
         </div>
-        <div class="foto-3">
+        <div class="foto-3" id="foto-3">
             <a href="#img-03"><img src="img/floreros/03.jpg" alt=""></a>
             <div class="descripcion">
                 <h3>hola</h3>
@@ -64,11 +64,11 @@
                     <a href=""><img src="img/floreros/03.jpg" alt=""></a>
                 </div>
                 <div class="cerrar">
-                    <a href="">x</a>
+                    <a href="#">x</a>
                 </div>
             </div>
         </div>
-        <div class="foto-4">
+        <div class="foto-4" id="foto-4">
             <a href="#img-04"><img src="img/floreros/04.jpg" alt=""></a>
             <div class="descripcion">
                 <h3>hola</h3>
@@ -79,7 +79,7 @@
                     <a href=""><img src="img/floreros/04.jpg" alt=""></a>
                 </div>
                 <div class="cerrar">
-                    <a href="">x</a>
+                    <a href="#">x</a>
                 </div>
             </div>
         </div>
@@ -87,7 +87,7 @@
 
 
     <div class="container-gallery">
-        <div class="foto-1">
+        <div class="foto-1" id="foto-5">
             <a href="#img-05"><img src="img/floreros/05.jpg" alt=""></a>
             <div class="descripcion">
                 <h3>hola</h3>
@@ -98,11 +98,11 @@
                     <a href=""><img src="img/floreros/05.jpg" alt=""></a>
                 </div>
                 <div class="cerrar">
-                    <a href="">x</a>
+                    <a href="#foto-5">x</a>
                 </div>
             </div>
         </div>
-        <div class="foto-2">
+        <div class="foto-2" id="foto-6">
             <a href="#img-06"><img src="img/floreros/06.jpg" alt=""></a>
             <div class="descripcion">
                 <h3>hola</h3>
@@ -113,11 +113,11 @@
                     <a href=""><img src="img/floreros/06.jpg" alt=""></a>
                 </div>
                 <div class="cerrar">
-                    <a href="">x</a>
+                    <a href="#foto-6">x</a>
                 </div>
             </div>
         </div>
-        <div class="foto-3">
+        <div class="foto-3" id="foto-7">
             <a href="#img-07"><img src="img/floreros/07.jpg" alt=""></a>
             <div class="descripcion">
                 <h3>hola</h3>
@@ -128,11 +128,11 @@
                     <a href=""><img src="img/floreros/07.jpg" alt=""></a>
                 </div>
                 <div class="cerrar">
-                    <a href="">x</a>
+                    <a href="#foto-7">x</a>
                 </div>
             </div>
         </div>
-        <div class="foto-4">
+        <div class="foto-4" id="foto-8">
             <a href="#img-08"><img src="img/floreros/08.jpg" alt=""></a>
             <div class="descripcion">
                 <h3>hola</h3>
@@ -143,14 +143,14 @@
                     <a href=""><img src="img/floreros/08.jpg" alt=""></a>
                 </div>
                 <div class="cerrar">
-                    <a href="">x</a>
+                    <a href="#foto-8">x</a>
                 </div>
             </div>
         </div>
     </div>
 
     <div class="container-gallery">
-        <div class="foto-1">
+        <div class="foto-1" id="foto-9">
             <a href="#img-09"><img src="img/floreros/09.jpg" alt=""></a>
             <div class="descripcion">
                 <h3>hola</h3>
@@ -161,11 +161,11 @@
                     <a href=""><img src="img/floreros/09.jpg" alt=""></a>
                 </div>
                 <div class="cerrar">
-                    <a href="">x</a>
+                    <a href="#foto-9">x</a>
                 </div>
             </div>
         </div>
-        <div class="foto-2">
+        <div class="foto-2" id="foto-10">
             <a href="#img-10"><img src="img/floreros/10.jpg" alt=""></a>
             <div class="descripcion">
                 <h3>hola</h3>
@@ -176,11 +176,11 @@
                     <a href=""><img src="img/floreros/10.jpg" alt=""></a>
                 </div>
                 <div class="cerrar">
-                    <a href="">x</a>
+                    <a href="#foto-10">x</a>
                 </div>
             </div>
         </div>
-        <div class="foto-3">
+        <div class="foto-3" id="foto-11">
             <a href="#img-11"><img src="img/floreros/11.jpg" alt=""></a>
             <div class="descripcion">
                 <h3>hola</h3>
@@ -191,11 +191,11 @@
                     <a href=""><img src="img/floreros/11.jpg" alt=""></a>
                 </div>
                 <div class="cerrar">
-                    <a href="">x</a>
+                    <a href="#foto-11">x</a>
                 </div>
             </div>
         </div>
-        <div class="foto-4">
+        <div class="foto-4" id="foto-12">
             <a href="#img-12"><img src="img/floreros/12.jpg" alt=""></a>
             <div class="descripcion">
                 <h3>hola</h3>
@@ -206,7 +206,7 @@
                     <a href=""><img src="img/floreros/12.jpg" alt=""></a>
                 </div>
                 <div class="cerrar">
-                    <a href="">x</a>
+                    <a href="#foto-12">x</a>
                 </div>
             </div>
     </div>
